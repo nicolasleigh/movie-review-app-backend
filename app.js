d7829463959f20553a41abaa03a5cdbb48fc32ac
NicolasLi <linze201@gmail.com>
2023-09-07 13:20:19 +0800

    The End of 27 -- Review API

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const { handleNotFound } = require('./utils/helper');
 const userRouter = require('./routes/user');
 const actorRouter = require('./routes/actor');
 const movieRouter = require('./routes/movie');
+const reviewRouter = require('./routes/review');
 
 const app = express();
 app.use(cors()); // for cross origin resource sharing
@@ -17,6 +18,7 @@ app.use(morgan('dev'));
 app.use('/api/user', userRouter);
 app.use('/api/actor', actorRouter);
 app.use('/api/movie', movieRouter);
+app.use('/api/review', reviewRouter);
 
 app.use('/*', handleNotFound);
 
