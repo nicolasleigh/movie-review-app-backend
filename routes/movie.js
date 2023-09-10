d4cf25b2a1bdbf2d4349dd70e8369d7bba0bca36
NicolasLi <linze201@gmail.com>
2023-09-10 13:08:06 +0800

    The End of 36 -- Finishing Up User UI

--- routes/movie.js
+++ routes/movie.js
@@ -13,6 +13,7 @@ const {
   getSingleMovie,
   getRelatedMovies,
   getTopRatedMovies,
+  searchPublicMovies,
 } = require('../controllers/movie');
 const { parseData } = require('../middlewares/helper');
 const {
@@ -74,5 +75,6 @@ router.get('/latest-uploads', getLatestUploads);
 router.get('/single/:movieId', getSingleMovie);
 router.get('/related/:movieId', getRelatedMovies);
 router.get('/top-rated', getTopRatedMovies);
+router.get('/search-public', searchPublicMovies);
 
 module.exports = router;
